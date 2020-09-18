fa87f62c41d9efe3396a620967ba264be5bd11f3
madfree <matzefrei@gmail.com>
2020-09-18 16:40:08 +0200

    feat: Add test for TasksViewModel
    - Add dependencies for AndroidX and Robolectric tests
    - Add LiveDataTestUtil helper class
    - Implement test for adding a task

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.ViewModelProvider
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import getOrAwaitValue
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.core.IsNot.not
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        // Given a fresh TasksViewModel
+        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        // When adding a new task
+        taskViewModel.addNewTask()
+
+        // Then the new task event is triggered
+        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
+
+    }
+}
